855a693759214ea6a42923eb6847b15f19525c29
Diba Mirza <dimirza@cs.ucsb.edu>
2023-02-08 13:40:32 -0800

    lect 09

--- lect09/test_bst.cpp
+++ lect09/test_bst.cpp
@@ -3,6 +3,17 @@
 #include <iostream>
 using namespace std;
 void test_removeNodeWithRightChild(){
+    bst b;
+    b.createSmallExample();
+    cout << "Preorder before deletion"<< endl;
+    b.printPreorder();
+    cout << endl;
+    b.removeNodeWithRightChild(b.root->left);
+    cout << "\tExpected : 50 30 100\n";
+    cout << "\tActual  : ";
+    b.printPreorder();
+    cout << endl;
+
 
 }
 
